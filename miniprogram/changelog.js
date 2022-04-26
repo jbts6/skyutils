01c220a7eafb5141089abce90a2a396382d35b1b
Analyzer <fh345392977@gmail.com>
2022-04-26 09:43:59 +0800

    Feat changelog

--- miniprogram/changelog.js
+++ miniprogram/changelog.js
@@ -1,11 +1,19 @@
 const changelog = [
   {
+    version: '1.1.1',
+    info: [
+      '支持更新日志'
+    ]
+  },
+  {
     version: '1.1.0',
-    info: '支持图片拼接'
+    info: [
+      '支持图片拼接'
+    ]
   },
   {
     version: '1.0.0',
-    info: '支持密码生成'
+    info: ['支持密码生成']
   }
 ]
 
