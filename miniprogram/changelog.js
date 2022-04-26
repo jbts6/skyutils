f6961874f878ad3ba94f35f2952a27b0ceba1204
Analyzer <fh345392977@gmail.com>
2022-04-26 09:46:00 +0800

    Feat add date

--- miniprogram/changelog.js
+++ miniprogram/changelog.js
@@ -1,18 +1,21 @@
 const changelog = [
   {
     version: '1.1.1',
+    date: '2022-04-14',
     info: [
       '支持更新日志'
     ]
   },
   {
     version: '1.1.0',
+    date: '2022-04-13',
     info: [
       '支持图片拼接'
     ]
   },
   {
     version: '1.0.0',
+    date: '2022-04-10',
     info: ['支持密码生成']
   }
 ]
